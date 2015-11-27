047c1a6bb334ef132d283b3cac513387e14ebc7e
jezzah <jezzah@gmail.com>
2015-11-27 18:12:01 -0500

    add mock json stuff for getting all expenses

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+var expenses = [
+  {
+    id: 1,
+    description: "first expense",
+    category:"automotive",
+    overdue: false
+  },
+  {
+    id: 2,
+    description: "second expense",
+    category:"home",
+    overdue: false
+  }
+];
 app
   .get('/', function(req, res) {
-  res.send('budget api');
+    res.send('budget api');
+})
+  .get('/expenses', function(req, res) {
+    res.json(expenses);
 })
   .listen(PORT, function() {
-  console.log('Express listening on port ' + PORT);
+    console.log('Express listening on port ' + PORT);
 });
