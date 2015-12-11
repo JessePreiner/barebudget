3153cccfecfd2f5b008693a9aec2b4f8b7e0c99b
manyourisms <jezzah@gmail.com>
2015-12-11 19:51:49 +0000

    Add DELETE route for expenses

--- budget/server.js
+++ budget/server.js
@@ -11,37 +11,50 @@ app.use(bodyParser.json());
 app
   .get('/', function(req, res) {
     res.send('budget api');
-})
+  })
   .get('/expenses/', function(req, res) {
     res.json(expenses);
-})
+  })
   .get('/expenses/:id', function(req, res) {
-    
+
     var id = parseInt(req.params.id, 10);
-    var result = _.findWhere(expenses, {id:id} );
+    var result = _.findWhere(expenses, {
+      id: id
+    });
     if (result) {
       res.json(result);
     }
     res.status(404).send();
-    
-    
-   })
-   .post('/expenses', function(req, res) {
-     var body = _.pick(req.body, 'description', 'completed');
-     
-     if (!_.isBoolean(body.completed) || 
-     !_.isString(body.description) || 
-     body.description.trim().length === 0) {
-       return res.status(400).send();
-     }
-     
-     body.id = expenseNextId++;
-     body.description = body.description.trim();
-     expenses.push(body);
-     res.json(body);
-   })
 
 
+  })
+  .post('/expenses', function(req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) ||
+      !_.isString(body.description) ||
+      body.description.trim().length === 0) {
+      return res.status(400).send();
+    }
+
+    body.id = expenseNextId++;
+    body.description = body.description.trim();
+    expenses.push(body);
+    res.json(body);
+  })
+
+  .delete('/expenses/:id', function(req, res) {
+    var body = req.body;
+    var id = parseInt(req.params.id, 10);
+    var expenseToRemove = _.findWhere(expenses, {id: id });
+    if (expenseToRemove) {
+      expenses = _.without(expenses, expenseToRemove);
+      res.json(expenseToRemove);
+    }
+    
+    return res.status(404).json({"error": "id was not found: " + id});
+  })
+  
   .listen(PORT, function() {
     console.log('Express listening on port ' + PORT);
-});
+  });
