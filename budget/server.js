6970ea707cfe98878bcfde32597c91da8acacffe
manyourisms <jezzah@gmail.com>
2015-12-11 19:32:20 +0000

    Refactor using underscore

--- budget/server.js
+++ budget/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,18 +16,27 @@ app
     res.json(expenses);
 })
   .get('/expenses/:id', function(req, res) {
-      var id = parseInt(req.params.id, 10);
-
-      expenses.forEach(function(x) {
-        if (x.id === id) {
-          res.json(x);
-        }
-      });
-      res.status(404).send();
+    
+    var id = parseInt(req.params.id, 10);
+    var result = _.findWhere(expenses, {id:id} );
+    if (result) {
+      res.json(result);
+    }
+    res.status(404).send();
+    
+    
    })
    .post('/expenses', function(req, res) {
-     var body = req.body;
+     var body = _.pick(req.body, 'description', 'completed');
+     
+     if (!_.isBoolean(body.completed) || 
+     !_.isString(body.description) || 
+     body.description.trim().length === 0) {
+       return res.status(400).send();
+     }
+     
      body.id = expenseNextId++;
+     body.description = body.description.trim();
      expenses.push(body);
      res.json(body);
    })
