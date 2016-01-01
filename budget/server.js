b86f5d850a3ff637128a1e1eddf10610c1923f21
Jesse Preiner <jezzah@gmail.com>
2016-01-01 15:46:30 -0600

    add update for expenses

--- budget/server.js
+++ budget/server.js
@@ -29,9 +29,9 @@ app
 
   })
   .post('/expenses', function(req, res) {
-    var body = _.pick(req.body, 'description', 'completed');
+    var body = _.pick(req.body, 'description', 'category');
 
-    if (!_.isBoolean(body.completed) ||
+    if (!_.isString(body.category) ||
       !_.isString(body.description) ||
       body.description.trim().length === 0) {
       return res.status(400).send();
@@ -43,6 +43,27 @@ app
     res.json(body);
   })
 
+  .put('/expenses/:id', function(req, res) {
+    var id = parseInt(req.params.id, 10);
+    var body = _.pick(req.body, 'description', 'category');
+    var expense = _.findWhere(expenses, {id: id });
+    var validAttributes = {};
+
+    if (body.hasOwnProperty('description') && _.isString(body.description)) {
+      validAttributes.description = body.description;
+    } else if (expense.hasOwnProperty(body.description)) {
+      res.status(400).send("Bad value for description");
+    }
+
+    if (body.hasOwnProperty('category') && _.isString(body.category)) {
+      validAttributes.category = body.category;
+    } else if (expense.hasOwnProperty(body.category)) {
+      res.status(400).send("Bad value for category");
+    }
+    _.extend(expense, validAttributes);
+    res.json(expense);
+  })
+
   .delete('/expenses/:id', function(req, res) {
     var body = req.body;
     var id = parseInt(req.params.id, 10);
@@ -51,10 +72,10 @@ app
       expenses = _.without(expenses, expenseToRemove);
       res.json(expenseToRemove);
     }
-    
+
     return res.status(404).json({"error": "id was not found: " + id});
   })
-  
+
   .listen(PORT, function() {
     console.log('Express listening on port ' + PORT);
   });
